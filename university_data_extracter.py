ff6f2523d5aa91c4f291424844c31dde9629fd54
piyushsoni27 <piy1995s@gmail.com>
2018-11-03 03:31:37 +0530

    added university_data_extracter.py

---
+++ university_data_extracter.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 01:25:02 2018
+
+@author: piyush
+"""
+
+import re
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import warnings
+import datetime
+
+default_region = "usa"
+
+class UniversityData():
+    
+    def __init__(self, region=None):
+        
+        if(region == None):
+            region = default_region
+        print("Extracting Data, please wait...\n")
+        
+        site = "https://yocket.in/universities/study-in-" + region
+        
+        self.university_df = pd.DataFrame(columns = ["Name", "link"])
+        
+        while(True):
+            page = self.load_page(site)
+                
+            soup = BeautifulSoup(page.content, "html.parser")
+    
+            self.get_university_data_current_page(soup)
+    
+            next_site = soup.find("li", {"class" : "next"}).find('a', href=True)["href"]
+
+            if(next_site == ""):
+                break
+            else:
+                site = "https://yocket.in" + next_site
+        
+        print(self.university_df)
+        
+    def load_page(self, site):
+        try:
+            page = requests.get(site)
+        except:
+            raise ConnectionError("Check your Internet Connection")
+    
+        return page
+        
+    def get_university_data_current_page(self, soup):
+        
+        university_list = soup.find_all("div", {"class" : "col-sm-9 col-xs-12"})
+        
+        for university in university_list:
+            university_link = "https://yocket.in" + university.find("a", href = True)["href"]
+            university_name = university.find("a", href = True).get_text()
+            
+            self.university_df = self.university_df.append({"Name" : university_name, "link" : university_link}, ignore_index = True)
+        
+       
+        
+        
+if __name__ == "__main__":
+    data = UniversityData("germany")
